46786481ea93d013bbb70f6818df43f55300e5ae
orencohendev <oren@orencreates.com>
2022-03-28 19:49:31 +0300

    adding validator

--- apps/notes-app/app.js
+++ apps/notes-app/app.js
@@ -1,5 +1,8 @@
+var validator = require('validator')
 const getNotes = require('./notes.js')
 
 
 console.log("App file loaded.")
-console.log(getNotes())
+var msg = getNotes()
+console.log(msg)
+console.log(validator.isURL("https://google.com"))
